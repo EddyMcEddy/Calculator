dfac429e154201e782d8c3ded8655888de5f677d
Eddy Huapaya <ehuap@yahoo.com>
2022-10-29 16:27:32 -0400

    else if statements

--- Program.cs
+++ Program.cs
@@ -2,11 +2,50 @@
 
 namespace Calculator
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Calculator");
+            Console.WriteLine("_____________________");
+
+            Console.Write("Enter first digit: ");
+            double userFirstDigit = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the sign you like: ");
+            var userSign = Console.ReadLine();
+
+            Console.WriteLine("Enter your second digit: ");
+            double userSecondDigit = Convert.ToDouble(Console.ReadLine());
+
+
+            if (userSign == "-")
+            {
+                Console.WriteLine(userFirstDigit - userSecondDigit);
+            }
+            else if (userSign == "+")
+            {
+                Console.WriteLine(userFirstDigit + userSecondDigit);
+            }
+            else if (userSign == "%")
+            {
+                Console.WriteLine(userFirstDigit % userSecondDigit);
+            }
+            else if (userSign == "*")
+            {
+                Console.WriteLine(userFirstDigit * userSecondDigit);
+            }
+            else if (userSign == "/")
+            {
+                Console.WriteLine(userFirstDigit / userSecondDigit);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid sign");
+            }
+
+        }
     }
-  }
 }
